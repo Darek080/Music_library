102d9435e3c88897d6aeee5750c3382cd396ecef
Darek_Bobek <darbob080@gmail.com>
2019-07-23 22:09:58 +0200

    0.2

--- music_reports.py
+++ music_reports.py
@@ -1,24 +1,32 @@
 
 list_of_all = []
-file_base = open('/home/darek/Pulpit/Music_library/text_albums_data.txt','r')
+file_base = open('text_albums_data.txt','r')
 for line in file_base.readlines():
     list_of_all.append(line.rstrip().split(','))
-    
-file_base.close()  
-
-
+ 
+file_base.close()
+ 
 def all_albums(list_of_all):
     for line in list_of_all:
         album = line[1]
         print(album)
     
+def albums_by_genre(list_of_all):
+    choise = input('Select the genre: ')
+    list_genre = []
+    for line in list_of_all:
+    
+        if choise in line[3]:
+            list_genre.append(line)
 
-def albums_by_genre():
-    pass
+    for ist in list_genre:
+        print(ist)
+    
 def albums_time_range():
     pass
 def shortest_longest_album():
     pass
 def albums_created_by_artist():
     pass
-all_albums(list_of_all)
+#print(list_of_all)
+albums_by_genre(list_of_all)
